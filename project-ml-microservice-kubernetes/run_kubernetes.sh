af24396bd5f7cbadaff9e2cd548c59f0284083bf
EC2 Default User <ec2-user@ip-172-31-30-73.us-west-1.compute.internal>
2022-09-06 20:44:26 +0000

    Deploy with Kubernetes and Save Output Logs

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -5,14 +5,14 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
-
+dockerpath=oumaimalmhamdi/housepriceprediction
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run housepriceprediction --image=$dockerpath
 
 # Step 3:
 # List kubernetes pods
-
+kubectl get pods --all-namespaces
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward housepriceprediction 8000:80
